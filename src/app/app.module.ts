ec4505725d06c0ba9badbed993b3671443ca5963
cagatay-usta <cagatay_usta@windowslive.com>
2023-10-29 16:26:35 +0300

    integrate angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { CigCounterComponent } from './counters/cig-counter/cig-counter.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const appRoutes: Routes = [
   { path: 'cig-counter', component: CigCounterComponent },
@@ -22,6 +27,11 @@ const appRoutes: Routes = [
       registrationStrategy: 'registerWhenStable:30000',
     }),
     RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    // Angular Material
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
