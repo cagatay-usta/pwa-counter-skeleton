e36dd1fce97a101826e48295f1ba11d053b96ce3
cagatay-usta <cagatay_usta@windowslive.com>
2023-11-12 11:49:56 +0300

    complete working import/export as txt-based json string

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 
+// TODO: convert retrievals to observables for live data sharing
+// TODO: refactor methods for general key/value pairs instead of specific items
+
 @Injectable({
   providedIn: 'root',
 })
@@ -16,24 +19,38 @@ export class StorageService {
   }
 
   exportFile() {
-    const data = JSON.stringify(localStorage);
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(
-      new Blob([data as BlobPart], { type: 'text/txt' })
-    );
-    a.download = 'export.txt';
-    a.click();
+    try {
+      const data = JSON.stringify(localStorage);
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(
+        new Blob([data as BlobPart], { type: 'text/txt' })
+      );
+      a.download = 'export.txt';
+      a.click();
+    } catch (error) {
+      console.error('Error exporting data: ', error);
+    }
   }
 
   importFile() {
     const input = document.createElement('input');
     input.type = 'file';
 
-    let importedData: string = '';
-
-    input.onchange = () => {
-      input.files?.[0].text().then((data) => console.log(typeof data));
-      console.log(importedData);
+    input.onchange = async () => {
+      if (input.files && input.files[0]) {
+        try {
+          const importedData = await input.files[0].text();
+          if (importedData) {
+            localStorage.clear();
+            const parsedData = JSON.parse(importedData);
+            Object.keys(parsedData).forEach((key) => {
+              localStorage.setItem(key, parsedData[key]);
+            });
+          }
+        } catch (error) {
+          console.error('Error importing data: ', error);
+        }
+      }
     };
 
     input.click();
