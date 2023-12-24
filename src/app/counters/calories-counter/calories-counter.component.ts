13d637c06a07b591cdbebd3805226dfa7d6cfec0
cagatay-usta <cagatay_usta@windowslive.com>
2023-12-24 14:38:10 +0300

    add basic layout for cal module

--- src/app/counters/calories-counter/calories-counter.component.ts
+++ src/app/counters/calories-counter/calories-counter.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+// TODO: track item usage to display common items
+// TODO: only display all items if clicked
+
 @Component({
   selector: 'app-calories-counter',
   templateUrl: './calories-counter.component.html',
-  styleUrls: ['./calories-counter.component.scss']
+  styleUrls: ['./calories-counter.component.scss'],
 })
 export class CaloriesCounterComponent {
+  calories: number = 0;
+  commonItems: string[] = [
+    'egg',
+    'ramen',
+    'cup-o-soup',
+    'pasta',
+    'yoghurt',
+    'wrap',
+    'rice',
+  ];
 
+  onDecreaseClick() {}
+
+  onIncreaseClick() {}
 }
